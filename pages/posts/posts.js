1d5d34f435d7ce75484596a729da07bf56d3253b
徐曙辉 <474497097@qq.com>
2017-03-30 11:48:34 +0800

    文章列表页增加数据循环

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,8 +1,34 @@
 // pages/posts/post.js
 Page({
-  data:{},
+  data:{
+   post_key:{}
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    var post_content=
+    [
+      {
+        date:"Jar 20 2017",
+        title:"正是虾肥蟹壮时",
+        content:"正是虾肥蟹壮时",
+        view_num:"112",
+        collect_num:"96",
+        author_img:"/images/avatar/1.png",
+        img_status:true,
+      },
+
+      {
+        date:"Jar 20 2017",
+        title:"1111",
+        content:"22222",
+        view_num:"112",
+        collect_num:"96",
+        author_img:"/images/avatar/1.png",
+        img_status:true,
+      }
+    ]
+    
+    this.setData({post_key:post_content});
   },
   onReady:function(){
     // 页面渲染完成
